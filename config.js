98cf8994e561a5ab161b60a5a0ce5ed54e3fd209
Dragos Adrian ROMANIUC <dromaniuc@bitdefender.com>
2019-08-27 23:11:28 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    routes: [
+        {route: "/performancemonitor", address: "http://localhost:8085"}
+    ]
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+
+var express = require('express');
+// const bodyParser = require('body-parser')
+// var proxy = require('http-proxy-middleware');
+// const routes = require('./config');
+const cors = require('cors');
+// const path = require('path');
+
+var servers = [
+    {host: "www.localhost.com", port: 80}
+]
+
+const app = express();
+// app.use(cors());
+
+// for (route of routes) {
+//     app.use(route.route,
+//         proxy({
+//             target: route.address,
+//             pathRewrite: (path, req) => {
+//                 return path.split('/').slice(2).join('/');
+//             }
+//         })
+//     );
+// }
+
+// app.listen(80, ()=>{
+//     console.log("Proxy listening on port 80");
+// })
+
+
+app.use(cors());
+
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+    res.send("success");
+});
+
+
+app.listen(process.env.PORT || 8085);
